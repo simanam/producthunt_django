c1503a829ad98c240887cba085c6c658dbfa82c5
Amandeep Singh <amansingh@Amandeeps-MacBook-Pro.local>
2018-10-18 13:05:50 -0700

    Second Commit

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-
-    path('', views.home, name='home'),
+    path('create', views.create, name='create'),
+    path('<int:product_id>', views.detail, name='detail'),
 ]
