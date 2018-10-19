739e32a17837b4b70c595af275ca9513f775f018
Amandeep Singh <amansingh@Amandeeps-MacBook-Pro.local>
2018-10-19 08:41:41 -0700

    finished app

--- products/models.py
+++ products/models.py
@@ -15,5 +15,8 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    def summary(self):
+        return self.body[:100]
+
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
